7c920a86561fc68ed0085d3c36eb4f52cb7d2bd8
key2-hue <keymaeda3782@gmail.com>
2020-03-30 19:42:08 +0900

    03/30 19:42

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>クイズアプリ</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <div id="container">
+    <h1>Q&A</h1>
+    <ul>
+      <li class="answer">0</li>
+      <li class="answer">1</li>
+      <li class="answer">2</li>
+      <li class="answer">3</li>
+      <li class="answer">4</li>
+    </ul>
+    <div id="btn" class="disabled">次の質問へ</div>
+  </div>
+</body>
+</html>
